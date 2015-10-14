d45a44111228735133e4b1e156ca2074750c91ee
olaf.goerlitz <olaf.goerlitz@gmail.com>
2015-10-14 21:46:58 +0200

    improved NQuad unit test

--- src/test/scala/NQuadUtilTest.scala
+++ src/test/scala/NQuadUtilTest.scala
@@ -11,50 +11,37 @@ class NQuadUtilTest extends FlatSpec with Matchers with BeforeAndAfterAll {
 
   val sc = new SparkContext("local", "CountTest")
 
-  val testQuads = Array("<http://cars.com/myCar> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://cars.com/Car> <http://cars.com> .")
+  "The NQuadUtil" should "parse valid NQuads correctly" in {
+    val validNQuads = """
+      _:Bob _:knows _:Tom _:MyFriends .
+      <http://ex.com/Bob> <http://ex.com/knows> "Tom" <http://ex.com/Friends> .
+      """.trim.split('\n').seq
 
-  "The NQuadUtil" should "parse a NQuad correctly" in {
-    val testQuadRDD = sc.parallelize(testQuads.seq)
-    val parsedRDD = NQuadUtil.parse(testQuadRDD)
+    val textRDD = sc.parallelize(validNQuads)
 
-    parsedRDD.count() shouldEqual (1)
+    val quadRDD = NQuadUtil.parse(textRDD)
+    quadRDD.count() shouldEqual (validNQuads.length)
 
-    parsedRDD.collect()(0) match {
-      case (s, p, o, c) =>
-        s shouldBe new Resource("http://cars.com/myCar")
-        p shouldBe new Resource("http://www.w3.org/1999/02/22-rdf-syntax-ns#type")
-        o shouldBe new Resource("http://cars.com/Car")
-        c shouldBe new Resource("http://cars.com")
-    }
-  }
-
-  it should "succeed another test" in {
-    val res = getClass.getResource("btc-2010-10k.gz")
-    val textRDD = sc.textFile(res.toString)
-    val parsedRDD = NQuadUtil.parse(textRDD)
-
-    //    val literals = parsedRDD.map { case (s, p, o, c) => o }.map { case lit: Literal => lit.getLabel }
-    val literals = parsedRDD.map { case (s, p, o, c) => o }.filter { case lit: Literal => true; case _ => false } map { x => x.getLabel }
-    println(literals.count())
+    val errors = NQuadUtil.getParseFailures(textRDD)
+    errors.count() shouldEqual (0)
   }
 
   it should "fail for invalid NQuads" in {
 
-    val examples = """
+    val invalidNQuads = """
       _:Bob .
       _:Bob _:knows .
       _:Bob _:knows _:Tom .
-      _:Bob _:knows _:Tom _:MyFriends . // the only correct one
-      """.trim().split("\n").seq
+      _:Bob _:knows _:Tom _:MyFriends _:Other .
+      """.trim.split('\n').seq
 
-    val textRDD = sc.parallelize(examples)
+    val textRDD = sc.parallelize(invalidNQuads)
 
     val quadRDD = NQuadUtil.parse(textRDD)
-    quadRDD.count() shouldEqual (1)
+    quadRDD.count() shouldEqual (0)
 
     val errors = NQuadUtil.getParseFailures(textRDD)
-    errors.count() shouldEqual (3)
-
+    errors.count() shouldEqual (invalidNQuads.length)
   }
 
   override def afterAll() = {
